4a2b7ec9bedc45993c398a958c283155bc0ee5de
mrbengutierrez <mrbengutierrez@gmail.com>
2018-08-08 10:42:51 -0400

    Updated License

--- neuralnet_02.py
+++ neuralnet_02.py
@@ -1,6 +1,9 @@
 """
+DESCRIPTION:
 This file contains a general purpose neural network that can be used for many
 applications
+
+See demo in in main()
 
 NOTE: Working activation fns: sigmoid, tanh, arctan, sin, gaussian, softplus
 
@@ -12,6 +15,29 @@ NOTE: trainTestSample(), trainWithPlots() ,testSample,,testBatch() come with
       By raising thres_high or lowering thres_low, you put a tighter
       bound on how accurate your neural net needs to be in order consider
       the testing sample has passed accurately.
+
+
+MIT License
+
+Copyright (c) 2018  The-Instrumental-Specialists
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 """
 
 import numpy as np # necessary unless want to rewrite
